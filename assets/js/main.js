7b304894313d79e3a6387832773a538352ed09a5
deformator852 <kostneko30@gmail.com>
2024-10-17 06:03:01 -0400

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".menu-item-has-children").hover(
+    function () {
+      $(".sub-menu").fadeIn(400);
+    },
+    function () {
+      $(".sub-menu").fadeOut(400);
+    }
+  );
+});

9a381fad6d59b4a4f3c36d41c02f0de61830ada2
deformator852 <kostneko30@gmail.com>
2024-10-27 03:38:18 -0400

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,20 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: false,
-        autoplaySpeed: 2000,
+        speed: 500,
         dots: false,
         arrows: true,
+        variableWidth: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>'
     });
+    $(".pet-card__image").hover(
+        function () {
+            let img = $(this).find("img");
+            img.css('scale', '110%');
+        },
+        function () {
+            let img = $(this).find("img");
+            img.css('scale', '100%');
+        })
 });

410e3e400e6d22f4eb8ca87caf0dc1edbd4d0824
deformator852 <kostneko30@gmail.com>
2024-11-02 16:40:57 -0400

    changes

--- functions.php
+++ functions.php
@@ -2,64 +2,62 @@
 
 class ThemeAssets
 {
-    public function enqueue_assets()
-    {
-        add_action('wp_enqueue_scripts', function () {
-            wp_enqueue_style('index', get_template_directory_uri() . '/assets/css/index.css');
-            wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
-            if (is_front_page()) {
-                wp_enqueue_style("home_css", get_template_directory_uri() . '/assets/css/pages/home.css');
-            }
-            // if (is_single()) {
-            //     wp_enqueue_style("single", get_template_directory_uri() . '/assets/css/pages/single.css');
-            // }
+  public function enqueue_assets()
+  {
+    add_action('wp_enqueue_scripts', function () {
+      wp_enqueue_style('index', get_template_directory_uri() . '/assets/css/index.css');
+      wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
+      wp_enqueue_style("poppins", "https://fonts.googleapis.com");
+      if (is_front_page()) {
+        wp_enqueue_style("home_css", get_template_directory_uri() . '/assets/css/pages/home.css');
+      }
+      if (is_single()) {
+        wp_enqueue_style("single", get_template_directory_uri() . '/assets/css/pages/single.css');
+      }
 
-            if (is_page_template("about.php")) {
-                wp_enqueue_style('about_page', get_template_directory_uri() . '/assets/css/pages/about.css');
-            }
-            if (is_page_template("puppies.php")) {
-                wp_enqueue_style("puppies_page", get_template_directory_uri() . '/assets/css/pages/puppies.css');
-            }
-            if (is_page_template("services.php")) {
-                wp_enqueue_style("services_page", get_template_directory_uri() . '/assets/css/pages/services.css');
-            }
-            if (is_page_template("contact.php")) {
-                wp_enqueue_style("contact_page", get_template_directory_uri() . '/assets/css/pages/contact.css');
-            }
+      if (is_page_template("about.php")) {
+        wp_enqueue_style('about_page', get_template_directory_uri() . '/assets/css/pages/about.css');
+      }
+      if (is_page_template("puppies.php")) {
+        wp_enqueue_style("puppies_page", get_template_directory_uri() . '/assets/css/pages/puppies.css');
+      }
+      if (is_page_template("services.php")) {
+        wp_enqueue_style("services_page", get_template_directory_uri() . '/assets/css/pages/services.css');
+      }
+      if (is_page_template("contact.php")) {
+        wp_enqueue_style("contact_page", get_template_directory_uri() . '/assets/css/pages/contact.css');
+      }
 
-            if (is_page_template("FAQs.php")) {
-                wp_enqueue_style("faqs_page", get_template_directory_uri() . '/assets/css/pages/faqs.css');
-            }
-            wp_enqueue_script("custom-jquery", get_template_directory_uri() . '/assets/js/jquery-3.7.1.min.js');
-            wp_enqueue_script("mainjs", get_template_directory_uri() . '/assets/js/main.js', array("custom-jquery"));
-            wp_enqueue_script('slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array('custom-jquery'));
+      if (is_page_template("FAQs.php")) {
+        wp_enqueue_style("faqs_page", get_template_directory_uri() . '/assets/css/pages/faqs.css');
+      }
+      wp_enqueue_script("custom-jquery", get_template_directory_uri() . '/assets/js/jquery-3.7.1.min.js');
+      wp_enqueue_script("mainjs", get_template_directory_uri() . '/assets/js/main.js', array("custom-jquery"));
+      wp_enqueue_script('slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array('custom-jquery'));
 
-        });
-    }
+    });
+  }
 
-    public function add_theme_supports()
-    {
-        add_theme_support('post-thumbnails');
-        add_theme_support('title-tag');
-        add_theme_support('custom-logo');
-    }
+  public function add_theme_supports()
+  {
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+  }
 
-    public function register_menus()
-    {
-        add_action("after_setup_theme", function () {
-            register_nav_menus([
-                "header-menu" => "Header menu",
-                "footer-menu" => "Footer menu"
-            ]);
-        });
-    }
-    
-
+  public function register_menus()
+  {
+    add_action("after_setup_theme", function () {
+      register_nav_menus([
+        "header-menu" => "Header menu",
+        "footer-menu" => "Footer menu"
+      ]);
+    });
+  }
 }
 
 $theme_assets = new ThemeAssets();
 $theme_assets->enqueue_assets();
 $theme_assets->add_theme_supports();
 $theme_assets->register_menus();
-
 
--- single.php
+++ single.php
@@ -1,33 +1,57 @@
+<?php include_once "components/red_button/red_button.php" ?>
 <?php get_header() ?>
 <main class="main">
     <div class="main__wrapper">
-        <?php get_header(); ?>
+      <?php get_header(); ?>
         <main class="main">
             <div class="main__wrapper">
-                <?php
-                // Start the loop to get the current post
-                if (have_posts()) :
-                    while (have_posts()) : the_post(); ?>
-                        <div class="acf-fields">
-                            <p><strong>Name:</strong> <?php echo esc_html(get_field('name')); ?></p>
-                            <p><strong>Gender:</strong> <?php echo esc_html(get_field('gender')); ?></p>
-                            <p><strong>Neutered:</strong> <?php echo esc_html(get_field('neutered')); ?></p>
-                            <p><strong>Age:</strong> <?php echo esc_html(get_field('age')); ?></p>
-                            <p><strong>Breed:</strong> <?php echo esc_html(get_field('breed')); ?></p>
-                            <p><strong>Vaccinated:</strong> <?php echo esc_html(get_field('vaccinated')); ?></p>
-                            <p><strong>Size:</strong> <?php echo esc_html(get_field('size')); ?></p>
-                            <p><strong>Brief
-                                    Description:</strong> <?php echo esc_html(get_field('brief_description')); ?></p>
-                            <p><strong>About:</strong> <?php echo esc_html(get_field('about')); ?></p>
+              <?php
+              if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
+                    <div class="single-pet">
+                        <div class="single-pet__information">
+                          <?= the_post_thumbnail($size = 'single-size', ['id' => 'single-pet__information-image']); ?>
+                            <div class="single-pet__description">
+                                <h2 class="single-pet__description-title">
+                                    Meet <?= esc_html(get_field('name')); ?>
+                                </h2>
+                                <div class="single-pet__description-detail">
+                                    <span><strong>Gender:</strong> <?= esc_html(get_field('gender')); ?></span>
+                                    <span><strong>Neutered:</strong> <?= esc_html(get_field('neutered')); ?></span>
+                                    <span><strong>Age:</strong> <?= esc_html(get_field('age')); ?></span>
+                                    <span><strong>Breed:</strong> <?= esc_html(get_field('breed')); ?></span>
+                                    <span><strong>Vaccinated:</strong> <?= esc_html(get_field('vaccinated')); ?></span>
+                                    <span><strong>Size:</strong> <?= esc_html(get_field('size')); ?><span/>
+                                </div>
+                                <div class="single-pet__description-brief-description">
+                                    <p><?= esc_html(get_field('brief_description')); ?></p>
+                                </div>
+                              <?php render_red_button("Apply Today", "link"); ?>
+                            </div>
                         </div>
+                        <div class="about">
+                            <div class="about__container">
+                                <h2>About <?= esc_html(get_field('name')); ?></h2>
+                                <p class="about__text">
+                                  <?= nl2br(esc_html(get_field('about'))); ?>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="adoption-rules">
+                            <div class="adoption-rules__container">
+                                <h2 class="adoption-rules__title">
+                                    <i class="fas fa-paperclip"></i>Adoption Rules
+                                </h2>
+                                <p class="adoption-rules__text"><?= esc_html(get_field('adoption_rules')) ?></p>
+                            </div>
+                        </div>
+                    </div>
 
-                    <?php endwhile;
-                else :
-                    echo '<p>No content found</p>'; // Message if no post is found
-                endif; ?>
+                <?php endwhile;
+              endif; ?>
             </div>
         </main>
-        <?php get_footer(); ?>
+      <?php get_footer(); ?>
 
     </div>
 </main>
